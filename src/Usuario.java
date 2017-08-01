fe21e98f89491cd850223eb7a4ff6d806f2d3154
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2017-08-01 19:21:01 -0300

    Update Usuario.java
    Att 01/08 - 19:20

--- src/Usuario.java
+++ src/Usuario.java
@@ -50,7 +50,7 @@ public class Usuario {
 	
 	@Override
 	public String toString() {
-		return "Usuario [nome=" + nome + ", email=" + email + ", celular=" + celular + "]";
+		return nome + ", " + email + ", " + celular;
 	}
 
 	@Override
