7bbacd611fad02ee6a72905f5dc8c3234d50441d
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2017-08-01 17:11:04 -0300

    Update Usuario.java

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,5 +1,86 @@
 package src;
 
+import java.util.ArrayList;
+
 public class Usuario {
 
+	private String nome;
+	private String email;
+	private String celular;
+	private ArrayList<Item> itens;
+	
+	public Usuario(String nome, String email, String celular) {
+		this.setNome(nome);
+		this.setEmail(email);
+		this.setCelular(celular);
+		setItens(new ArrayList<>());
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public ArrayList<Item> getItens() {
+		return itens;
+	}
+
+	public void setItens(ArrayList<Item> itens) {
+		this.itens = itens;
+	}
+	
+	@Override
+	public String toString() {
+		return "Usuario [nome=" + nome + ", email=" + email + ", celular=" + celular + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((celular == null) ? 0 : celular.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (celular == null) {
+			if (other.celular != null)
+				return false;
+		} else if (!celular.equals(other.celular))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
 }
