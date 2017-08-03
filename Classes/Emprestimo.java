76dc44b3e55c8403220144f7baf70a74b853fb33
wesleylq <wesley.lucena.queiroz@ccc.ufcg.edu.br>
2017-08-03 11:10:16 -0300

    att classe emprestimo

--- Classes/Emprestimo.java
+++ Classes/Emprestimo.java
@@ -10,7 +10,7 @@ public class Emprestimo {
 	private Item item;
 	private int tempoEmprestimo;
 	
-	private Calendar dataInicial = Calendar.getInstance();
+	private Calendar dataInicial = Calendar.getInstance().DAY_OF_YEAR;
 	private Calendar dataFinal = Calendar.getInstance();
 	
 	/**
@@ -28,6 +28,54 @@ public class Emprestimo {
 		this.tempoEmprestimo = tempoEmprestimo;
 		
 		dataFinal.add(Calendar.DATE, tempoEmprestimo); 
+	}
+
+	public Usuario getDonoItem() {
+		return donoItem;
+	}
+
+	public void setDonoItem(Usuario donoItem) {
+		this.donoItem = donoItem;
+	}
+
+	public Usuario getUsuarioEmprestado() {
+		return usuarioEmprestado;
+	}
+
+	public void setUsuarioEmprestado(Usuario usuarioEmprestado) {
+		this.usuarioEmprestado = usuarioEmprestado;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	public int getTempoEmprestimo() {
+		return tempoEmprestimo;
+	}
+
+	public void setTempoEmprestimo(int tempoEmprestimo) {
+		this.tempoEmprestimo = tempoEmprestimo;
+	}
+
+	public Calendar getDataInicial() {
+		return dataInicial;
+	}
+
+	public void setDataInicial(Calendar dataInicial) {
+		this.dataInicial = dataInicial;
+	}
+
+	public Calendar getDataFinal() {
+		return dataFinal;
+	}
+
+	public void setDataFinal(Calendar dataFinal) {
+		this.dataFinal = dataFinal;
 	}
 	
 	
