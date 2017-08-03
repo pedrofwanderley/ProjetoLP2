838458991e950d6d210c7457145c3eb9f289c971
Pedro Farias <pedro.wanderley@ccc.ufcg.edu.br>
2017-08-03 10:59:42 -0300

    Controllerpesquisa atualizado

--- Classes/ControllerPesquisa.java
+++ Classes/ControllerPesquisa.java
@@ -4,7 +4,7 @@ public class ControllerPesquisa {
 	
 	public UsuarioController usuarios = new UsuarioController();
 	
-	public String listaItensUsuarios(){
+	public String listaItensUsuariosNome(){
 		NomeItemComparator comparator = new NomeItemComparator();
 		Collections.sort(usuarios.getItensTotais(), comparator);
 		String listaItens = "";
@@ -15,5 +15,21 @@ public class ControllerPesquisa {
 		
 		return listaItens;
 	}
+	
+	public String listaItensUsuariosValor(){
+		ValorItemComparator comparator = new ValorItemComparator();
+		Collections.sort(usuarios.getItensTotais(),comparator);
+		
+		String listaItens  = "";
+		for (int i = 0; i < usuarios.getItensTotais().size(); i++) {
+			listaItens += usuarios.getItensTotais().get(i).toString() + System.lineSeparator();
+			
+		}
+		
+		return listaItens;
+	}
+	
+	
+	
 
 }
