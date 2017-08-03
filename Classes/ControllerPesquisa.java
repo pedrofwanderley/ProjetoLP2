b1e25b232afa2aaeaf1f2fad3267320670f0851a
Pedro Farias <pedro.wanderley@ccc.ufcg.edu.br>
2017-08-03 11:46:57 -0300

    ControllerPesquisa atualizada

--- Classes/ControllerPesquisa.java
+++ Classes/ControllerPesquisa.java
@@ -1,35 +1,38 @@
 import java.util.Collections;
+import java.util.List;
+
+import itens.Item;
 
 public class ControllerPesquisa {
-	
-	public UsuarioController usuarios = new UsuarioController();
-	
-	public String listaItensUsuariosNome(){
+
+	public UsuarioController usuarios;
+
+	public ControllerPesquisa() {
+		this.usuarios = new UsuarioController();
+	}
+
+	public String listaItensUsuariosNome(List<Item> itens) {
 		NomeItemComparator comparator = new NomeItemComparator();
-		Collections.sort(usuarios.getItensTotais(), comparator);
+		Collections.sort(itens, comparator);
 		String listaItens = "";
-		for (int i = 0; i < usuarios.getItensTotais().size(); i++) {
-			listaItens += usuarios.getItensTotais().get(i).toString() + System.lineSeparator();
-			
+		for (int i = 0; i < itens.size(); i++) {
+			listaItens += itens.get(i).toString() + System.lineSeparator();
+
 		}
-		
+
 		return listaItens;
 	}
-	
-	public String listaItensUsuariosValor(){
+
+	public String listaItensUsuariosValor(List<Item> itens) {
 		ValorItemComparator comparator = new ValorItemComparator();
-		Collections.sort(usuarios.getItensTotais(),comparator);
-		
-		String listaItens  = "";
-		for (int i = 0; i < usuarios.getItensTotais().size(); i++) {
-			listaItens += usuarios.getItensTotais().get(i).toString() + System.lineSeparator();
-			
+		Collections.sort(itens, comparator);
+		String listaItens = "";
+		for (int i = 0; i < itens.size(); i++) {
+			listaItens += itens.get(i).toString() + System.lineSeparator();
+
 		}
-		
+
 		return listaItens;
 	}
-	
-	
-	
 
 }
