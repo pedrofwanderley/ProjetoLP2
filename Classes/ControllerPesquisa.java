a15d759fc757557b6dcf6b3bbd23ee1da9b67a45
PEDRO FARIAS WANDERLEY 116110151 <pedrofw@lcc2-38.lcc.ufcg.edu.br>
2017-08-03 14:57:35 -0300

    Classe comentada

--- Classes/ControllerPesquisa.java
+++ Classes/ControllerPesquisa.java
@@ -1,19 +1,23 @@
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import itens.Item;
+
 /**
  * Classe de controle de pesquisas.
+ * 
  * @author pedrofw
  *
  */
 public class ControllerPesquisa {
-
-
+	
 	public ControllerPesquisa() {
 	}
-
+	/**
+	 * Método de ordenacao e exibicao de itens em ordem alfabetica.
+	 * @param itens, o parametro é uma lista de itens.
+	 * @return, retorna as representacoes dos itens em forma de string.
+	 */
 	public String listaItensUsuariosNome(List<Item> itens) {
 		NomeItemComparator comparator = new NomeItemComparator();
 		Collections.sort(itens, comparator);
@@ -25,7 +29,11 @@ public class ControllerPesquisa {
 
 		return listaItens;
 	}
-
+	/**
+	 * Metodo de ordenacao e exibicao de itens por ordem de valor.
+	 * @param itens, o parametro é uma lista de itens
+	 * @return, retorna uma representacao em string de todos os itens dos usuários.
+	 */
 	public String listaItensUsuariosValor(List<Item> itens) {
 		ValorItemComparator comparator = new ValorItemComparator();
 		Collections.sort(itens, comparator);
@@ -37,7 +45,6 @@ public class ControllerPesquisa {
 
 		return listaItens;
 	}
-	
 	
 	
 
