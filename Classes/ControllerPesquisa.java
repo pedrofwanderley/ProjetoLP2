3bbb4cfb4df2a55cbd78493209744d883ee1973c
Pedro Wanderley <pedro.wanderley@ccc.ufcg.edu.br>
2017-08-05 22:52:54 -0300

    Pesquisas e ordenacao FINALIZADAS

--- Classes/ControllerPesquisa.java
+++ Classes/ControllerPesquisa.java
@@ -13,12 +13,19 @@ import itens.Item;
  */
 public class ControllerPesquisa {
 	
-	private List<Item> itensSistema = new ArrayList<>();
 	
 	public ControllerPesquisa() {
 	}
-
 		
+	public List<Item> geraListaItens(Map<String,Usuario> usuarios){
+		List<Item> itensSistema = new ArrayList<>();
+		for (Usuario usuario : usuarios.values()){
+			for(Item item : usuario.getItens().values()){
+				itensSistema .add(item);
+			}
+		}
+		return itensSistema;
+	}
 	
 	/**
 	 * Método de ordenacao e exibicao de itens em ordem alfabetica.
@@ -26,19 +33,14 @@ public class ControllerPesquisa {
 	 * @return, retorna as representacoes dos itens em forma de string.
 	 */
 	public String listaItensUsuariosNome(Map<String,Usuario> usuarios) {
-		for (Usuario usuario : usuarios.values()){
-			for(Item itens : usuario.getItens().values()){
-				itensSistema .add(itens);
-			}
-		}
+		List<Item> itens = geraListaItens(usuarios);
 		NomeItemComparator comparator = new NomeItemComparator();
-		Collections.sort(itensSistema , comparator);
+		Collections.sort(itens, comparator);
 		String listaItens = "";
-		for (int i = 0; i < itensSistema .size(); i++) {
-			listaItens += itensSistema .get(i).toString() + "|";
+		for (int i = 0; i < itens.size(); i++) {
+			listaItens += itens.get(i).toString() + "|";
 
 		}
-
 		return listaItens;
 	}
 	/**
@@ -47,16 +49,12 @@ public class ControllerPesquisa {
 	 * @return, retorna uma representacao em string de todos os itens dos usuários.
 	 */
 	public String listaItensUsuariosValor(Map<String,Usuario> usuarios) {
-		for (Usuario usuario : usuarios.values()){
-			for(Item itens : usuario.getItens().values()){
-				itensSistema .add(itens);
-			}
-		}
+		List<Item> itens = geraListaItens(usuarios);
 		ValorItemComparator comparator = new ValorItemComparator();
-		Collections.sort(itensSistema, comparator);
+		Collections.sort(itens, comparator);
 		String listaItens = "";
-		for (int i = 0; i < itensSistema .size(); i++) {
-			listaItens += itensSistema .get(i).toString() + "|";
+		for (int i = 0; i < itens.size(); i++) {
+			listaItens += itens.get(i).toString() + "|";
 
 		}
 
@@ -65,10 +63,14 @@ public class ControllerPesquisa {
 	
 	public String pesquisarDetalhesItem(Map<String,Usuario> usuarios, String nome, String telefone, String nomeitem){
 		String itemDetalhado = "";
+		int confereUsuario = 0;
+		int confereItem = 0;
 		for (Usuario usuario : usuarios.values()){
 			if (nome.equals(usuario.getNome()) && telefone.equals(usuario.getCelular())) {
+				confereUsuario++;
 				for(Item itens : usuario.getItens().values()){
 					if (nomeitem.equals(itens.getNomeItem())){
+						confereItem++;
 						itemDetalhado = itens.toString();
 					}
 					
@@ -78,6 +80,14 @@ public class ControllerPesquisa {
 			
 			
 		}
+		if (confereUsuario == 0) {
+			throw new IllegalArgumentException("Usuario invalido");
+			
+		}
+		else if(confereItem == 0){
+			throw new IllegalArgumentException("Item nao encontrado");
+		}
+		
 		return itemDetalhado;
 	}
 	
