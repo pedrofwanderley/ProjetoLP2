e53fd57dc605b46af2d6d0f5d77441f3c792887c
wesleylq <wesley.lucena.queiroz@ccc.ufcg.edu.br>
2017-08-17 10:14:43 -0300

    Update Historico.java

--- Classes/usuario/Historico.java
+++ Classes/usuario/Historico.java
@@ -1,10 +1,7 @@
 package usuario;
-/**
- * @author Wesley
- */
-import java.util.Calendar;
 
-import E.SituacaoEmprestimo;
+
+import emprestimo.SituacaoEmprestimo;
 import itens.Item;
 
 public class Historico {
@@ -13,17 +10,16 @@ public class Historico {
 	private Item item;
 	private SituacaoEmprestimo situacao;
 	private String dataDevolucao;
-	private String dataFinal;
 	private int diasAtrasados;
 	
 	
-	public Historico(Usuario usuario, Item item, SituacaoEmprestimo situacao, String dataFinal ,String dataDevolucao){
+	public Historico(Usuario usuario, Item item, SituacaoEmprestimo situacao, String dataDevolucao, int diasAtrasados){
 		
 		this.usuario=usuario;
 		this.item=item;
 		this.situacao=situacao;
 		this.dataDevolucao = dataDevolucao;
-		this.dataFinal= dataFinal;
+		this.diasAtrasados= diasAtrasados;
 				
 	}
 	
@@ -31,36 +27,8 @@ public class Historico {
 		return item;
 	}
 
-	/**
-	 * Calcula os dias atrasados que o item foi devolvido
-	 * @param dataFinal
-	 * @param dataDevolucao
-	 * @return
-	 */
-	private int getDiasAtrasados(String dataFinal,String dataDevolucao){
-		
-		Calendar calendarFinal = Calendar.getInstance();
-		String[] datasFinal = dataFinal.split("/");
-		calendarFinal.set(Integer.parseInt(datasFinal[2]),Integer.parseInt(datasFinal[1]), Integer.parseInt(datasFinal[0]));
-		
-		
-		Calendar calendarDev = Calendar.getInstance();
-		String[] datasDev = dataDevolucao.split("/");
-		calendarDev.set(Integer.parseInt(datasDev[2]),Integer.parseInt(datasDev[1]), Integer.parseInt(datasDev[0]));
-		
-		
-		diasAtrasados = calendarFinal.get(Calendar.DAY_OF_YEAR) - calendarDev.get(Calendar.DAY_OF_YEAR);
-		
-		if(diasAtrasados > 0){
-			return diasAtrasados;
-		}else{
-			return 0;
-		}
-		
-		
-	}
 	
 	public String toString(){
-		return usuario + " - " + item + " - " + situacao+ " - " + dataDevolucao + " - " + getDiasAtrasados(dataFinal,dataDevolucao); 
+		return usuario + " - " + item + " - " + situacao+ " - " + dataDevolucao + " - " + diasAtrasados; 
 	}
 }
