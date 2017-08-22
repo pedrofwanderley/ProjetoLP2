24f06831a71e841a22e1f592851418e41fb9e6a5
wesleylq <wesley.lucena.queiroz@ccc.ufcg.edu.br>
2017-08-22 08:53:59 -0300

    Add files via upload

---
+++ Classes/pesquisa/MaiorReputacaoComparator.java
@@ -0,0 +1,14 @@
+package pesquisa;
+
+import java.util.Comparator;
+import usuario.Usuario;
+
+public class MaiorReputacaoComparator implements Comparator<Usuario> {
+	
+	public int compare(Usuario usuario1, Usuario usuario2) {
+		Double reputacao1 = usuario1.getReputacao(); 
+		Double reputacao2 = usuario2.getReputacao();
+		return reputacao2.compareTo(reputacao1);
+	}
+
+}
---
+++ Classes/pesquisa/MenorReputacaoComparator.java
@@ -0,0 +1,15 @@
+package pesquisa;
+
+import java.util.Comparator;
+
+import usuario.Usuario;
+
+public class MenorReputacaoComparator implements Comparator<Usuario> {
+	
+	public int compare(Usuario usuario1, Usuario usuario2) {
+		Double reputacao1 = usuario1.getReputacao(); 
+		Double reputacao2 = usuario2.getReputacao();
+		return reputacao1.compareTo(reputacao2);
+	}
+
+}
